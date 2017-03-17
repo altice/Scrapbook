01873b9d8e7370296411aaf4b0dffd352c6d71f2
ksaluja <ksaluja@haystax.com>
2017-03-17 14:50:47 -0400

    More

--- d3_notes.js
+++ d3_notes.js
@@ -1,3 +1,19 @@
+<script d3-array.min.js.
+<script src = 
+
+
+<style>
+
+line, path, .domain, .bar {
+	stroke:
+}
+</style>
+
+var data = [
+	{ 	key:'A', value:1,
+		key:'B', value:2}
+]
+
 g.append('g')
 	.attr('transform','translate('+ x(1) + ',0)')
 	.attr('class','axis')
@@ -18,4 +34,42 @@ holder.append('circle')
 		.attr('r',50)
 		.attr('stroke','red')
 		.style('fill', 'none')
-		
+
+.attr('r', function (){ return d.r})
+.call(drag()
+		.on('start', dragstarted)
+		.on('drag', dragended)	
+		.on('end(', dragendedn )	
+
+data.forEach(function(d){
+	d.date = parseTime(d.date)
+	d.close = +d.close
+})
+
+var scaleX = d3.scaleBand()
+	.domain(data.map ( function(d){ return d.key;}))
+	.range(0, chartWidth)
+	.paddingInner(.1)
+	.paddingOuter(.3)
+	.align(.5) // centering it
+
+var scaleY = d3.scale.scaleLinear()
+				.domain([0,d3_array.max(data.map(function(d){return d.value;})) )
+				.range(0,chartHeight)
+
+
+
+var xaxis = d3.axisBottom(scaleX)
+				.tickSizeInner(4)
+				.tickSizeOuter(20)
+				.tickPadding(3)
+
+panel.selectAll('rect.bar')
+	.data(data)
+	.enter().append('rect')
+	.attr('class', 'bar')
+	.attr('width',scaleX.bandwidth())
+	.attr('height', function(d) { return scaleX(d.key);})
+	.attr('y', function(d){ return -scaleY(d.value)})
+
+
