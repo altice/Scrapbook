8ae2957f6ab855847fbcb2c7ec858af461a472bc
ksaluja <ksaluja@haystax.com>
2017-03-17 13:19:29 -0400

    More

--- d3_notes.js
+++ d3_notes.js
@@ -1,5 +1,21 @@
 g.append('g')
-	.attr('transform','translate(12,0')
+	.attr('transform','translate('+ x(1) + ',0)')
 	.attr('class','axis')
 	.call(d3.axisLeft(y)
-			.ticks(20))
+			.ticks(20)
+			.tickFormat(".0s")));
+
+g.append('g'
+	.attr('transform', 'translate(' + x(2) + ',0)')
+	.attr('class', 'axis')
+	.call(d3.axisLeft(y)
+			.ticks(20)
+			.tickFormat(d3.formatPrefix('.1',1e6))
+
+holder.append('circle')
+		.attr('cx',200)
+		.attr('cy',100)
+		.attr('r',50)
+		.attr('stroke','red')
+		.style('fill', 'none')
+		
